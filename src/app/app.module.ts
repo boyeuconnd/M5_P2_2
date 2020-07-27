aee9877c16c0686a7774e12d6f5daec794e6c8f6
Admin <thienvu453@gmail.com>
2020-07-27 11:15:21 +0700

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontsizechanceComponent } from './fontsizechance/fontsizechance.component';
+import { PetinfoComponent } from './petinfo/petinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontsizechanceComponent,
+    PetinfoComponent
   ],
   imports: [
     BrowserModule
